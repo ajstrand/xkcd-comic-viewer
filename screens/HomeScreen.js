6b79cc9cd2d0decabbddfbe39ebd3d822cdbd17a
Alex <ajstrand8@gmail.com>
2018-12-30 14:36:58 -0500

    working on viewing comics in a list. titles aren't rendering at the moment

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
   Button,
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
   View,
+  KeyboardAvoidingView
 } from 'react-native';
 import ComicScreen from './ComicScreen'
 import { MonoText } from '../components/StyledText';
@@ -55,7 +55,6 @@ export default class HomeScreen extends React.Component {
         console.error(error);
       });
   }
-   
   }
 
  setNum(text) {
@@ -68,28 +67,46 @@ export default class HomeScreen extends React.Component {
     return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
+  renderButton() {
+    let result;
+    const ComicButton = <Button title="get comic" onPress={() => {this.getSpecificComic(this.state.inputNum)}}></Button> ;
+    const message = <Text>Please enter a number to get a specific comic.</Text>;
+    this.state.inputNum !== null ? result = ComicButton
+      : result = message
+      return result; 
+  }
+
+  getView() {
+    let view;
+    if(this.state.data !== null){
+      view =  <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <View style={styles.welcomeContainer}>
+        <ComicScreen data={this.state.data}/>
+      </View>
+      <Text>Enter a number to get a specfic comic</Text>
+      <TextInput 
+             style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={(text) => {this.setNum(text)}}
+  value={this.state.text}/>
+   {this.renderButton()}
+    </ScrollView>
+    }
+    else {
+      view = <Text>Loading</Text>;
+    }
+    return view;
+  }
+
 
 
   render() {
-    if(this.state.data !== null){
       return (
+        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
         <View style={styles.container}>
-          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-            <View style={styles.welcomeContainer}>
-              <ComicScreen data={this.state.data}/>
-            </View>
-            <TextInput 
-                   style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(text) => {this.setNum(text)}}
-        value={this.state.text}/>
-            <Button title="get comic" onPress={() => {this.getSpecificComic(this.state.inputNum)}}></Button>
-          </ScrollView>
+          {this.getView()}
         </View>
+        </KeyboardAvoidingView>
       );
-    }
-    else {
-      return (<View><Text>Loading</Text></View>)
-    }
     
   }
 }
@@ -99,86 +116,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
-    paddingTop: 30,
+    paddingTop: 10,
   },
   welcomeContainer: {
     alignItems: 'center',
     marginTop: 10,
-    marginBottom: 20,
   },
   comicImage: {
     width: 300,
-    height: 300,
+    height: 100,
     resizeMode: 'contain',
-    marginTop: 3,
+    marginTop: 1,
     marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
   },
   getStartedText: {
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     lineHeight: 24,
     textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  }
 });
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,18 +1,56 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
-    title: 'Links',
+    title: 'All Comics',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      titles: [{key:"2", title:"foo"}]
+    }
+  }
+
+  componentDidMount() {
+    for (let i = 1; i < 10; i++) {
+      this.getSpecificComic(i);
+    }
+  }
+
+
+  getSpecificComic(number) {
+    let url = `https://xkcd.com/${number}/info.0.json`
+    return fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let titlesCopy = this.state.titles.slice();
+        let o = { title: responseJson.title, key: number.toString() }
+        titlesCopy.push(o);
+        this.setState({ titles: titlesCopy });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  createList() {
+    let r;
+    this.state.titles.length > 0 ? r = <FlatList
+      data={this.state.titles}
+      renderItem={({ item }) => <Text key={item.key}>{item.title}</Text> } /> : r = <Text>Placeholder</Text>
+    console.log(r)
+    return r;
+  }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
+      {this.createList()}
+        {/* <ComicScreen data={this.state.data}/> */}
       </ScrollView>
     );
   }
